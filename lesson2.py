f72f04572f7f335b30d1613352a100b69c9c6c1a
Odysseus-Abraham Kirikopoulos <odysseuskirikopoulos@gmail.com>
2022-10-23 15:26:43 +0300

    Initial Commit

---
+++ lesson2.py
@@ -0,0 +1,23 @@
+#Variables - Μεταβλητές
+
+name = "odysseus" #Characters - Χαρακτήρες (string)
+
+#name
+#variable's name - το όνομα της μεταβλητής
+
+# = "odysseus"
+#value - τιμή
+
+number = 100 #Numbers - Νούμερα (integers)
+
+boolians = False #True or False - Αληθές ή Ψευδές (boolian)
+
+#How to print a variable - Πως να τυπόσω μία μεταβλητή
+
+print(number)
+
+#Changing a value - Αλλαγή μιας τιμής
+
+number = 101
+
+print(number)
